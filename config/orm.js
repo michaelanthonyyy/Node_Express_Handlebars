5371565b4b8f501b653b6e0fd99598a895a7d800
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-01 15:33:20 -0800

    create burger added in orm.js to mirror burger.js file in models folder

--- config/orm.js
+++ config/orm.js
@@ -9,7 +9,20 @@ var orm = {
             }
             cb(result);
         })
-    }
+    },
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO ?? (??) VALUES ?";
+
+        console.log(queryString);
+
+        connection.query(queryString, table, cols, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
+    },
+    update: function()
 }
 
 
