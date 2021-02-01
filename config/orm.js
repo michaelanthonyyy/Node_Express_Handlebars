70dcc5e52ceb8848fb3bd0aa8d574a4c5401d923
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-01 14:58:55 -0800

    orm display all function added in orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,18 @@
 var connection = require ("/connection.js");
 
-
-
-
-
 var orm = {
-    
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
+    }
 }
+
+
+
+
+module.exports = orm;
