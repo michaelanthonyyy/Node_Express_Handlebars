683cbc40d00cfd07e4ffd4ffdd3a64e5486c9ae0
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-01 15:42:15 -0800

    added update function to orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,41 @@
-var connection = require ("/connection.js");
+var connection = require("/connection.js");
 
 var orm = {
-    all: function(tableInput, cb) {
+    all: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function(err, result) {
+        connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
             }
             cb(result);
         })
     },
-    create: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO ?? (??) VALUES ?";
+    create: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table + " (" + cols.toString() + ") " + "VALUES (" + printQuestionMarks(vals.length) + ") ";
 
         console.log(queryString);
 
-        connection.query(queryString, table, cols, vals, function(err, result) {
+        connection.query(queryString, vals, function (err, result) {
             if (err) {
                 throw err;
             }
+
             cb(result);
-        })
+        });
     },
-    update: function()
+    update: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table + " SET " + objToSql(objColVals) + " WHERE " + condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
 }
 
 
