d27f471342eff08fa79b701f46684c54f9e2166d
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-01 21:09:26 -0800

    index.handlebars file changes made. asset image added

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-sleep").on("click", function(event) {
+      var id = $(this).data("id");
+      var devoured = $(this).data("devoured");
+  
+      var devouredState = {
+        devour: devoured
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devouredState
+      }).then(
+        function() {
+          console.log("changed sleep to", devouredState);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+      var newBurger = {
+        name: $("#burgername").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new cat");
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+        $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+          location.reload();
+        }
+      );
+    });
+  });
+  
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,16 +15,16 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function (results) {
+    burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function (result) {
         res.json({ id: result.insertId });
     });
 });
 
-router.put("/api/burger/:id", function (req, res) {
+router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
     console.log("condition", condition);
     burger.update({
-        devour: req.body.devour
+        devoured: req.body.devoured
     }, condition, function (result) {
         if (result.changedRows == 0) {
             return res.status(404).end();
@@ -39,7 +39,6 @@ router.delete("/api/burgers/:id", function (req, res) {
 
     burger.delete(condition, function (result) {
         if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         } else {
             res.status(200).end();
